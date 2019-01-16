1fe4272a25aefdec9c8d5f1b678c6d34a8f7394a
Elias Calderon <eliascr98@outlook.com>
2019-01-16 08:22:42 -0600

    Comments

--- js/main.js
+++ js/main.js
@@ -6,13 +6,14 @@ var EXPERIENCE_INDEX = 3;
 var SKILLS_INDEX = 4;
 var PROJECTS_INDEX = 5;
 var CONTACT_INDEX = 6;
-var WAYPOINT_OFFSET = 50;
+var WAYPOINT_OFFSET = 64;
 var WAYPOINT_OFFSET_SUBSECTION = 80;
 
-/* Variables */
+/* Progress Bar Arrays */
 var basicProgressBar = [];
 var intermediateProgressBar = [];
 
+/* Animate the basic and the intermediate progress bars */
 function animateProgressBars() {
   basicProgressBar.forEach(function (element) {
     element.animate(0.25);
@@ -23,31 +24,33 @@ function animateProgressBars() {
   });
 }
 
+/* Remove the previously active navbar-item and set the new one with the index parameter */
 function setActiveNavItem(index) {
   $('.nav-item.active').removeClass('active');
   $('.nav-item').eq(index).addClass('active');
 }
 
+/* Function that will run once the entire page (images or iframes), not just the DOM, is ready. */
 $(window).on('load', function () {
   $('.nav-link:not(#contact-item)').bind('click', function (e) {
-    e.preventDefault(); // prevent hard jump, the default behavior
+    e.preventDefault(); // Prevent hard jump to section
 
-    var target = $(this).attr("href"); // Set the target as variable
+    var target = $(this).attr("href"); // Get the section to jump to
 
-    // perform animated scrolling by getting top-position of target-element and set it as scroll target
+    // Animated scroll, set the position to go to, to the position of the section
     $('html, body').stop().animate({
       scrollTop: $(target).offset().top
     }, 600, function () {
-      location.hash = target; //attach the section id to the pageurl
+      location.hash = target; // Attach the section id to the explorer bar
     });
 
     return false;
   });
 
   $('#contact-item').bind('click', function (e) {
-    e.preventDefault(); // prevent hard jump, the default behavior
+    e.preventDefault();
 
-    // perform animated scrolling to the bottom of page
+    // Animate the scroll to the bottom of the webpage
     $('html, body').stop().animate({
       scrollTop: $(document).height()
     }, 600);
@@ -55,9 +58,11 @@ $(window).on('load', function () {
     return false;
   });
 
+  // Show the home title
   $('.home-title').eq(0).addClass('animated fadeIn slow');
 });
 
+/* Function that qwill only run once the page DOM is ready */
 $(document).ready(function () {
 
   const progressBarStyle = {
@@ -71,19 +76,22 @@ $(document).ready(function () {
     }
   };
 
+  // Skill names
   const intermediateSkillNames = ['#javaBar', '#cppBar', '#springBar', '#hibernateBar', '#mavenBar', '#gitBar', '#sqlBar'];
   const basicSkillNames = ['#csharpBar', '#pythonBar', '#cBar', '#htmlBar', '#jsBar', '#cssBar', '#jerseyBar', '#lambdaBar',
-  '#apigBar', '#dynamoBar', '#cognitoBar', '#iamBar'];
+    '#apigBar', '#dynamoBar', '#cognitoBar', '#iamBar'];
 
 
-  intermediateSkillNames.forEach( function(name) {
+  // For each skill push the progress bars to the arrays
+  intermediateSkillNames.forEach(function (name) {
     intermediateProgressBar.push(new ProgressBar.Line(name, progressBarStyle));
-  }); 
+  });
 
-  basicSkillNames.forEach( function(name) {
+  basicSkillNames.forEach(function (name) {
     basicProgressBar.push(new ProgressBar.Line(name, progressBarStyle));
   });
 
+  // waypoint configuration
   new Waypoint({
     element: document.getElementById('about-me'),
     handler: function (direction) {
@@ -92,7 +100,7 @@ $(document).ready(function () {
         $('.navbar.bg-dark').removeClass('bg-dark');
         $('.navbar').addClass('navbar-light');
         $('.navbar').addClass('bg-light');
-  
+
         setActiveNavItem(HOME_INDEX);
         pJSDom[0].pJS.particles.move.enable = true;
         pJSDom[0].pJS.fn.particlesRefresh();
@@ -102,7 +110,7 @@ $(document).ready(function () {
         $('.navbar.bg-light').removeClass('bg-light');
         $('.navbar').addClass('navbar-dark');
         $('.navbar').addClass('bg-dark');
-  
+
         setActiveNavItem(ABOUT_INDEX);
         pJSDom[0].pJS.particles.move.enable = false;
         $('#about-me-left-animated').addClass('animated fadeInLeft slow');
@@ -112,7 +120,7 @@ $(document).ready(function () {
     },
     offset: WAYPOINT_OFFSET
   });
-  
+
   new Waypoint({
     element: document.getElementById('education'),
     handler: function (direction) {
@@ -127,7 +135,7 @@ $(document).ready(function () {
     },
     offset: WAYPOINT_OFFSET
   });
-  
+
   new Waypoint({
     element: document.getElementById('experience'),
     handler: function (direction) {
@@ -143,7 +151,7 @@ $(document).ready(function () {
     },
     offset: WAYPOINT_OFFSET
   });
-  
+
   new Waypoint({
     element: document.getElementById('skills'),
     handler: function (direction) {
@@ -157,7 +165,7 @@ $(document).ready(function () {
     },
     offset: WAYPOINT_OFFSET
   });
-  
+
   new Waypoint({
     element: document.getElementById('technical-skills'),
     handler: function () {
@@ -165,7 +173,7 @@ $(document).ready(function () {
     },
     offset: WAYPOINT_OFFSET_SUBSECTION
   });
-  
+
   new Waypoint({
     element: document.getElementById('projects'),
     handler: function (direction) {
@@ -182,117 +190,117 @@ $(document).ready(function () {
     offset: WAYPOINT_OFFSET
   });
 
-/* Particles Configuration */
+  // Particles.js Configuration
 
-particlesJS("home", {
-  "particles": {
-    "number": {
-      "value": 200,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#22A39F"
-    },
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#000000"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.8,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 10,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 10,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": false,
-      "distance": 500,
-      "color": "#ffffff",
-      "opacity": 0.4,
-      "width": 2
-    },
-    "move": {
-      "enable": true,
-      "speed": 2,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": true,
-        "mode": "repulse"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 0.5
+  particlesJS("home", {
+    "particles": {
+      "number": {
+        "value": 200,
+        "density": {
+          "enable": true,
+          "value_area": 800
         }
       },
-      "bubble": {
-        "distance": 400,
-        "size": 4,
-        "duration": 0.3,
-        "opacity": 1,
-        "speed": 3
+      "color": {
+        "value": "#22A39F"
       },
-      "repulse": {
-        "distance": 100,
-        "duration": 0.4
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
       },
-      "push": {
-        "particles_nb": 4
+      "opacity": {
+        "value": 0.8,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
       },
-      "remove": {
-        "particles_nb": 2
+      "size": {
+        "value": 10,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 10,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 500,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 2
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
       }
-    }
-  },
-  "retina_detect": true
-});
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 0.5
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 4,
+          "duration": 0.3,
+          "opacity": 1,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 100,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
 
 });
